6b50454bbf180bb1351b1f60115acbadc847e9fe
guisin77 <guisin77@gmail.com>
2017-09-15 15:31:47 +0900

    layout add

---
+++ app/src/main/java/com/yang/myalarm/AlarmDetailActivity.java
@@ -0,0 +1,13 @@
+package com.yang.myalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_detail);
+    }
+}
---
+++ app/src/main/java/com/yang/myalarm/SoundFromPCActivity.java
@@ -0,0 +1,13 @@
+package com.yang.myalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoundFromPCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_from_pc);
+    }
+}
